c30febd332a8cfe21afa7537db79974a3cf857bb
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-24 12:20:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
- document.getElementById('text').innerHTML = "This is really
-cool!";
+ document.getElementById('text').innerHTML = "This is really cool!";
 });
